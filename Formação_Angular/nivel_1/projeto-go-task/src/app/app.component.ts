dd31cc4651088a022b67a724032eed86cd99d51a
Murilo Miranda Verçosa <murilo.vercosa@centralinf.local>
2025-11-19 14:51:07 -0300

    antes de iniciar o 4 modulo

--- Formação_Angular/nivel_1/projeto-go-task/src/app/app.component.ts
+++ Formação_Angular/nivel_1/projeto-go-task/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './_components/header/header.component';
 import { MainContentComponent } from './_components/main-content/main-content.component';
+import { TaskFormModalComponent } from './_components/task-form-modal/task-form-modal.component';
+import { TaskCommentModalComponent } from './_components/task-comment-modal/task-comment-modal.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, MainContentComponent],
+  imports: [
+    HeaderComponent,
+    MainContentComponent,
+    TaskFormModalComponent,
+    TaskCommentModalComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
