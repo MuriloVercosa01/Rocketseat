37415a20811f2e1443f5f60299448a779c30eb27
Murilo Miranda Verçosa <murilo.vercosa@centralinf.local>
2025-12-19 10:25:51 -0300

    modulo 5/Configuração do Drag and Drop nas colunas de status da tarefa

--- Formação_Angular/nivel_1/projeto-go-task/src/app/components/task-list-section/task-list-section.component.ts
+++ Formação_Angular/nivel_1/projeto-go-task/src/app/components/task-list-section/task-list-section.component.ts
@@ -1,19 +1,58 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { TaskCardComponent } from '../task-card/task-card.component';
 import { taskService } from '../../services/task.service';
+import {
+  CdkDrag,
+  CdkDragDrop,
+  CdkDropList,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
+import { ITask } from '../../interfaces/task.interface';
 
 @Component({
   selector: 'app-task-list-section',
-  imports: [TaskCardComponent],
+  imports: [TaskCardComponent, CdkDropList, CdkDrag],
   templateUrl: './task-list-section.component.html',
   styleUrl: './task-list-section.component.css',
 })
 export class TaskListSectionComponent implements OnInit {
+  todoTasks: ITask[] = [];
+
+  doingTasks: ITask[] = [];
+
+  doneTasks: ITask[] = [];
+
   private readonly _taskService = inject(taskService);
 
   ngOnInit(): void {
-    this._taskService.todoTasks.subscribe((todoList) => {
-      console.log('Lista de todos: ', todoList);
+    this._taskService.todoTasks.subscribe((todoTasks) => {
+      this.todoTasks = todoTasks;
     });
+
+    this._taskService.doingTasks.subscribe((doingTasks) => {
+      this.todoTasks = doingTasks;
+    });
+
+    this._taskService.doneTasks.subscribe((doneTasks) => {
+      this.doneTasks = doneTasks;
+    });
+  }
+
+  drop(event: CdkDragDrop<ITask[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
   }
 }
